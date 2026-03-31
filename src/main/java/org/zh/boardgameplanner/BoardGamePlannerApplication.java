96edd163712c5133ddaea70fa3e0630606d092cd
yaxianzh <zhang.yaxia@northeastern.edu>
2026-03-30 22:34:53 -0700

    link UI to full collection.json and enable search

--- src/main/java/org/zh/boardgameplanner/BoardGamePlannerApplication.java
+++ src/main/java/org/zh/boardgameplanner/BoardGamePlannerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardGamePlannerApplication {
-    // 启动 Web 服务
     public static void main(String[] args) {
         SpringApplication.run(BoardGamePlannerApplication.class, args);
     }
